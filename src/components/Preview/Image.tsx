32764d468f42730cc8d7844d10e97bc814fdf9d6
Kshitij Pandey <kshitijpandey32@gmail.com>
2025-02-17 13:04:03 +1100

    remove lazy loading for images

--- src/components/Preview/Image.tsx
+++ src/components/Preview/Image.tsx
@@ -3,12 +3,7 @@ import { PreviewImage } from "../../lib/types";
 function Image({ caption, url }: Readonly<PreviewImage>) {
 	return (
 		<div className="md:mr-4">
-			<img
-				src={url}
-				alt={caption}
-				className="max-w-full h-auto w-full"
-				loading="lazy"
-			/>
+			<img src={url} alt={caption} className="max-w-full h-auto w-full" />
 		</div>
 	);
 }
